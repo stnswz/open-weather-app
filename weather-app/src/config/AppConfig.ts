9f3ab4bd3526a1a8fa0ebc633843895fb9c43a84
Steffen Schwarz <stn_swz@gmx.net>
2020-02-05 16:33:23 +0100

    Renames attribute

--- weather-app/src/config/AppConfig.ts
+++ weather-app/src/config/AppConfig.ts
@@ -1,11 +1,11 @@
 interface IAppConfig {
     HOST:string,
-    APPID:string,
+    API_KEY:string,
     ICON_URL:string,
 }
 
 export const AppConfig:IAppConfig = {
     HOST: "http://api.openweathermap.org/data/2.5/forecast",
-    APPID: "ca58313c1aca8a7d3f65508931f39677",
+    API_KEY: "ca58313c1aca8a7d3f65508931f39677", // Get your own API key on https://openweathermap.org/guide#how
     ICON_URL: "http://openweathermap.org/img/wn/",
 }
