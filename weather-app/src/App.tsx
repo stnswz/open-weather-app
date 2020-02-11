5acd445db51e04d2da98979eb19b71275afcc28f
Steffen Schwarz <stn_swz@gmx.net>
2020-02-11 03:03:36 +0100

    Adds resize functionality

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -4,12 +4,33 @@ import SelectionBar from "./components/SelectionBar";
 import WeatherContent from "./components/WeatherContent";
 import './css/App.css';
 
-class App extends Component {
+
+interface IState {
+    height: number,
+}
+interface IProps {
+    /** empty */
+}
+
+class App extends Component<IProps, IState>  {
+
+    constructor(props:IProps) {
+        super(props);
+        this.state = {
+            height: 0,
+        }
+    }
+
+    public componentDidMount() {
+        window.addEventListener('resize', (ev:any) => this.setState({height: window.innerHeight}) );
+    }
 
     public render(): ReactElement {
 
+        let heightStyle = this.state.height !== 0 ? {height:this.state.height+"px"} : {};
+
         return (
-            <div id="mainContent">
+            <div id="mainContent" style={heightStyle}>
                 <div id="innerColumn">
                     <div id="innerContent">
 
