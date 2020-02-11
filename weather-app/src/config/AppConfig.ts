24a02e9a0c8cc0bdb1cdf3d70a8172067c48b017
Steffen Schwarz <stn_swz@gmx.net>
2020-02-11 17:21:56 +0100

    Updates URLs

--- weather-app/src/config/AppConfig.ts
+++ weather-app/src/config/AppConfig.ts
@@ -5,7 +5,7 @@ interface IAppConfig {
 }
 
 export const AppConfig:IAppConfig = {
-    HOST: "http://api.openweathermap.org/data/2.5/forecast",
+    HOST: "https://api.openweathermap.org/data/2.5/forecast",
     API_KEY: "ca58313c1aca8a7d3f65508931f39677", // Get your own API key on https://openweathermap.org/guide#how
-    ICON_URL: "http://openweathermap.org/img/wn/",
+    ICON_URL: "https://openweathermap.org/img/wn/",
 }
